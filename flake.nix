9a469d8dc7e5eadf007c919b52d75d51c5b8a6f0
Matteo Pacini <matteo@codecraft.it>
2024-06-15 08:44:36 +0100

    nixos-efi-format: fixed not a string error

--- flake.nix
+++ flake.nix
@@ -23,27 +23,29 @@
       apps.aarch64-linux = {
         nixos-efi-format = {
           type = "app";
-          program = linuxPkgs.writeShellScriptBin "efi-format" ''
-            #!${linuxPkgs.bash}/bin/bash
-            export PATH=${linuxPkgs.gptfdisk}/bin:$PATH
-            sudo sgdisk \
-              -og $1 \
-              -n 1:0:+512M \
-              -t 1:ef00 \
-              -n 2:0:+4G \
-              -t 2:8200 \
-              -n 3:0:0 \
-              -t 3:8300 \
-              $1
-            sudo mkfs.vfat -F 32 ''${1}1
-            sudo mkswap ''${1}2
-            sudo swapon ''${1}2
-            sudo mkfs.xfs ''${1}3
-            sudo mount /dev/''${1}3 /mnt
-            sudo mkdir /mnt/boot
-            sudo mount /dev/''${1}1 /mnt/boot
-            sudo nixos-generate-config --root /mnt
-          '';
+          program = toString (
+            linuxPkgs.writeShellScriptBin "efi-format" ''
+              #!${linuxPkgs.bash}/bin/bash
+              export PATH=${linuxPkgs.gptfdisk}/bin:$PATH
+              sudo sgdisk \
+                -og $1 \
+                -n 1:0:+512M \
+                -t 1:ef00 \
+                -n 2:0:+4G \
+                -t 2:8200 \
+                -n 3:0:0 \
+                -t 3:8300 \
+                $1
+              sudo mkfs.vfat -F 32 ''${1}1
+              sudo mkswap ''${1}2
+              sudo swapon ''${1}2
+              sudo mkfs.xfs ''${1}3
+              sudo mount /dev/''${1}3 /mnt
+              sudo mkdir /mnt/boot
+              sudo mount /dev/''${1}1 /mnt/boot
+              sudo nixos-generate-config --root /mnt
+            ''
+          );
         };
       };
     };
